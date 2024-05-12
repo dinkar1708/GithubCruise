89a437a0020af8b80f39ff2e5d341f6a705688ef
Maurya <dinkar1708@gmail.com>
2024-05-13 00:28:01 +0900

    Test case design

---
+++ app/src/test/java/com/jetpack/compose/github/github/cruise/domain/usecase/SearchRepositoryUseCaseTest.kt
@@ -0,0 +1,20 @@
+package com.jetpack.compose.github.github.cruise.domain.usecase
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class SearchRepositoryUseCaseTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun searchUsers() {
+    }
+}
---
+++ app/src/test/java/com/jetpack/compose/github/github/cruise/ui/features/users/UsersListViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.jetpack.compose.github.github.cruise.ui.features.users
+
+import org.junit.After
+import org.junit.Before
+
+
+/**
+ * Created by Dinakar Maurya on 2024/05/13
+ */
+class UsersListViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
