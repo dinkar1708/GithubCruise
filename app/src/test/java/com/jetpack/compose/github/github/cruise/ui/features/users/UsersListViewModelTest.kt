369e0c8990bcb67f227674ac19d8e5e39321c5fd
Maurya <dinkar1708@gmail.com>
2024-05-13 18:11:39 +0900

    empty input test case

--- app/src/test/java/com/jetpack/compose/github/github/cruise/ui/features/users/UsersListViewModelTest.kt
+++ app/src/test/java/com/jetpack/compose/github/github/cruise/ui/features/users/UsersListViewModelTest.kt
@@ -1,7 +1,19 @@
 package com.jetpack.compose.github.github.cruise.ui.features.users
 
+import com.jetpack.compose.github.github.cruise.domain.model.SearchUser
+import com.jetpack.compose.github.github.cruise.domain.model.User
+import com.jetpack.compose.github.github.cruise.domain.usecase.SearchRepositoryUseCase
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 
 
 /**
@@ -9,12 +21,40 @@ import org.junit.Before
  */
 // TODO
 class UsersListViewModelTest {
+    private val mockSearchRepositoryUseCase: SearchRepositoryUseCase = mockk()
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var viewModel: UsersListViewModel
+
+    // mock data
+    private val user = User(
+        id = 1,
+        login = "dinkar1708",
+        type = "User",
+        avatarUrl = "https://avatars.githubusercontent.com/u/14831652?v=4",
+    )
+    private val searchUser = SearchUser(2, true, mutableListOf(user))
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        viewModel = UsersListViewModel(mockSearchRepositoryUseCase, testDispatcher)
     }
 
     @After
     fun tearDown() {
+        Dispatchers.resetMain()
     }
+
+    @Test
+    fun `search user - empty input and state error`() = runTest {
+        // When
+        // empty text
+        viewModel.updateInputString("")
+        advanceUntilIdle() // Advance time to process the flow
+        // Then
+        val state = viewModel.uiState.value
+        Assert.assertEquals(false, state.isLoading)
+        Assert.assertEquals("Input user name to search", state.errorMessage)
+    }
+
 }
