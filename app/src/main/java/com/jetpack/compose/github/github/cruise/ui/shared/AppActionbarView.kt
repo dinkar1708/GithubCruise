fa456106831aca313253515ee6530d03f83b24ea
Maurya <dinkar1708@gmail.com>
2024-05-13 13:12:25 +0900

    common app bar view + text color style example

---
+++ app/src/main/java/com/jetpack/compose/github/github/cruise/ui/shared/AppActionbarView.kt
@@ -0,0 +1,92 @@
+package com.jetpack.compose.github.github.cruise.ui.shared
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jetpack.compose.github.github.cruise.ui.theme.GithubCruiseTheme
+
+/**
+ * Created by Dinakar Maurya on 2024/05/13.
+ */
+@Composable
+fun AppActionBarView(
+    modifier: Modifier,
+    headerText: String,
+    showBackButton: Boolean = true,
+    onBackClick: (() -> Unit) = {}
+) {
+    Box(
+        modifier = modifier
+            .shadow(elevation = 1.dp)
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(0.dp)
+            ) {
+                if (showBackButton) {
+                    IconButton(
+                        onClick = { onBackClick() }, modifier = Modifier.size(30.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(30.dp),
+                        )
+                    }
+                }
+
+                Text(
+                    text = headerText,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        color = MaterialTheme.colorScheme.onBackground,
+                    ),
+                    modifier = if (showBackButton) Modifier
+                        .padding(horizontal = 40.dp)
+                        .fillMaxWidth() else Modifier.fillMaxWidth()
+                )
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenCommonHeaderPreview() {
+    GithubCruiseTheme {
+        Surface {
+            AppActionBarView(modifier = Modifier.fillMaxWidth(),
+                headerText = "Text",
+                showBackButton = false,
+                onBackClick = {})
+        }
+    }
+}
