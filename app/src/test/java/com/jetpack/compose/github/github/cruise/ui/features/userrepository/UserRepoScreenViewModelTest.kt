14acc39b8e187fbdc200ddd2f3bdee1c1433951a
Maurya <dinkar1708@gmail.com>
2024-05-17 02:11:30 +0900

    user repository view model test case fix + new

--- app/src/test/java/com/jetpack/compose/github/github/cruise/ui/features/userrepository/UserRepoScreenViewModelTest.kt
+++ app/src/test/java/com/jetpack/compose/github/github/cruise/ui/features/userrepository/UserRepoScreenViewModelTest.kt
@@ -2,6 +2,7 @@ package com.jetpack.compose.github.github.cruise.ui.features.userrepository
 
 import com.jetpack.compose.github.github.cruise.domain.model.User
 import com.jetpack.compose.github.github.cruise.domain.model.UserProfile
+import com.jetpack.compose.github.github.cruise.domain.model.UserRepo
 import com.jetpack.compose.github.github.cruise.domain.usecase.UserRepositoryUseCase
 import com.jetpack.compose.github.github.cruise.network.model.ApiError
 import com.jetpack.compose.github.github.cruise.network.model.ApiErrorResponse
@@ -26,6 +27,9 @@ class UserRepoScreenViewModelTest {
     private val testDispatcher = StandardTestDispatcher()
     private lateinit var viewModel: UserRepoScreenViewModel
 
+    // mock data
+    private  val pageNumber = 1
+    private  val pageSize = 40
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
@@ -38,47 +42,55 @@ class UserRepoScreenViewModelTest {
     }
 
     @Test
-    fun `test loadApiData uiStateProfile - loading cancelled after api call failed`() = runTest {
-        // mock data
-        val user = User(
-            id = 1,
-            login = "dinkar1708",
-            type = "User",
-            avatarUrl = "https://avatars.githubusercontent.com/u/14831652?v=4",
-        )
+    fun `test loadApiData() for uiStateProfile & uiStateRepository state and message on api call failed`() = runTest {
         // Given
-        coEvery { mockUserRepositoryUseCase.getUserProfile(user.login) } throws ApiError.ApiException(
+        coEvery { mockUserRepositoryUseCase.getUserProfile("dinkar1708") } throws ApiError.ApiException(
             ApiErrorResponse(
                 message = "API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
                 documentationUrl = "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"
             )
         )
+        coEvery { mockUserRepositoryUseCase.filterUserRepositories(false, "dinkar1708", pageNumber, pageSize) } throws ApiError.ApiException(
+            ApiErrorResponse(
+                message = "API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
+                documentationUrl = "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"
+            )
+        )
+
         // When
         // empty text
-        viewModel.loadApiData(user)
+        viewModel.loadApiData("dinkar1708")
 
         // Advance time to process the flow
         advanceUntilIdle()
-        val state = viewModel.uiStateProfile.value
+
         // Then
+
+        // test profile
+        val stateProfile = viewModel.uiStateProfile.value
         // checking loading without mock data
-        Assert.assertEquals(false, state.isLoading)
+        Assert.assertEquals(false, stateProfile.isLoading)
         // Network error is added from view model so lets test it.
         Assert.assertEquals(
-            "Network error: API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
-            state.errorMessage
+            "API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
+            stateProfile.errorMessage
+        )
+
+        // test repository
+        val stateRepository = viewModel.uiStateRepository.value
+        // checking loading without mock data
+        Assert.assertEquals(false, stateRepository.isLoading)
+        // Network error is added from view model so lets test it.
+        Assert.assertEquals(
+            "API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
+            stateRepository.errorMessage
         )
     }
 
     @Test
-    fun `test loadApiData uiStateProfile -api call success`() = runTest {
+    fun `test loadApiData() for uiStateProfile & uiStateRepository data on api call success`() = runTest {
         // mock data
-        val user = User(
-            id = 1,
-            login = "dinkar1708",
-            type = "User",
-            avatarUrl = "https://avatars.githubusercontent.com/u/14831652?v=4",
-        )
+        val login = "dinkar1708"
         val userProfile = UserProfile(
             id = 1,
             followers = 10,
@@ -87,94 +99,94 @@ class UserRepoScreenViewModelTest {
             avatarUrl = "url",
             login = "dinkar1708"
         )
+        val userRepoList = mutableListOf(
+            UserRepo(
+                id = 1,
+                name = "Repo",
+                language = "JAVA",
+                stargazersCount = "10",
+                description = "Android Repo Desc",
+                fork = false
+            ),
+            UserRepo(
+                id = 1,
+                name = "1 Repo",
+                language = "JAVA",
+                stargazersCount = "10",
+                description = "2 Android Repo Desc",
+                fork = false
+            ),
+        )
         // Given
-        coEvery { mockUserRepositoryUseCase.getUserProfile(user.login) } returns flowOf(userProfile)
+        coEvery { mockUserRepositoryUseCase.filterUserRepositories(false, login, pageNumber, pageSize) } returns flowOf( userRepoList)
+        // Given
+        coEvery { mockUserRepositoryUseCase.getUserProfile(login) } returns flowOf(userProfile)
+
         // When
-        // empty text
-        viewModel.loadApiData(user)
+        viewModel.loadApiData(login)
 
         // Advance time to process the flow
         advanceUntilIdle()
-        val state = viewModel.uiStateProfile.value
+
         // Then
+
+        // test user profile
+        val stateProfile = viewModel.uiStateProfile.value
         // checking loading cancelled
-        Assert.assertEquals(false, state.isLoading)
+        Assert.assertEquals(false, stateProfile.isLoading)
         // check that got the user profile in the state
-        Assert.assertEquals(userProfile, state.userProfile)
+        Assert.assertEquals(userProfile, stateProfile.userProfile)
+
+        // test user repository
+        val stateRepository = viewModel.uiStateRepository.value
+        // checking loading cancelled
+        Assert.assertEquals(false, stateRepository.isLoading)
+        // check that got same data in the state
+        Assert.assertEquals(userRepoList.size, stateRepository.userRepoList.size)
     }
 
-// TODO fix
-//
-//    @Test
-//    fun `test loadApiData filterNotForkedUserRepositories - loading cancelled after api call failed`() = runTest {
-//        // mock data
-//        val user = User(
-//            id = 1,
-//            login = "dinkar1708",
-//            type = "User",
-//            avatarUrl = "https://avatars.githubusercontent.com/u/14831652?v=4",
-//        )
-//        // Given
-//        coEvery { mockUserRepositoryUseCase.filterNotForkedUserRepositories(user.login, 1, 20) } throws ApiError.ApiException(
-//            ApiErrorResponse(
-//                message = "API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
-//                documentationUrl = "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"
-//            )
-//        )
-//        // When
-//        // empty text
-//        viewModel.loadApiData(user)
-//
-//        // Advance time to process the flow
-//        advanceUntilIdle()
-//        val state = viewModel.uiStateRepository.value
-//        // Then
-//        // checking loading without mock data
-//        Assert.assertEquals(false, state.isLoading)
-//        // TODO fix below error
-//        // Expected :API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.
-//        // Actual   :Network error: no answer found for: UserRepositoryUseCase(#1).filterNotForkedUserRepositories(dinkar1708, 1, 40, continuation {})
-//        Assert.assertEquals("API rate limit exceeded for 134.180.235.148. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.",
-//            state.errorMessage)
-//    }
 
-    // TODO fix
-//
-//    @Test
-//    fun `test loadApiData uiStateRepository -api call success`() = runTest {
-//        // mock data
-//        val user = User(
-//            id = 1,
-//            login = "dinkar1708",
-//            type = "User",
-//            avatarUrl = "https://avatars.githubusercontent.com/u/14831652?v=4",
-//        )
-//        val userRepoList = mutableListOf(
-//            UserRepo(
-//                owner = UserRepo.Owner(login = "dinakr1708", avatarUrl = "url"),
-//                id = 1,
-//                name = "Repo",
-//                language = "JAVA",
-//                stargazersCount = "10",
-//                description = "Android Repo Desc",
-//                fork = true
-//            ),
-//        )
-//        // Given
-//        // TODO fix below error
-//        // no answer found for: UserRepositoryUseCase(#1).filterNotForkedUserRepositories(dinkar1708, 1, 40, continuation {})
-//        coEvery { mockUserRepositoryUseCase.filterNotForkedUserRepositories(user.login, 1, 10) } returns flowOf( userRepoList)
-//        // When
-//        // empty text
-//        viewModel.loadApiData(user)
-//
-//        // Advance time to process the flow
-//        advanceUntilIdle()
-//        val state = viewModel.uiStateRepository.value
-//        // Then
-//        // checking loading cancelled
-//        Assert.assertEquals(false, state.isLoading)
-//        // check that got same data in the state
-//        Assert.assertEquals(userRepoList.size, state.userRepoList.size)
-//    }
+    @Test
+    fun `test filterRepositories() for uiStateRepository data on api call success`() = runTest {
+        // mock data
+        val login = "dinkar1708"
+
+        val userRepoList = mutableListOf(
+            UserRepo(
+                id = 1,
+                name = "Repo",
+                language = "JAVA",
+                stargazersCount = "10",
+                description = "Android Repo Desc",
+                fork = false
+            ),
+            UserRepo(
+                id = 1,
+                name = "1 Repo",
+                language = "JAVA",
+                stargazersCount = "10",
+                description = "2 Android Repo Desc",
+                fork = false
+            ),
+        )
+        // Given
+        // NOTE this time passing true for is showing for repo because our function[filterRepositories] is changing its value
+        coEvery { mockUserRepositoryUseCase.filterUserRepositories(true, login, pageNumber, pageSize) } returns flowOf( userRepoList)
+
+        // When
+        viewModel.filterRepositories(true, login)
+
+        // Advance time to process the flow
+        advanceUntilIdle()
+
+        // Then
+
+        // test user repository
+        val stateRepository = viewModel.uiStateRepository.value
+        // checking loading cancelled
+        Assert.assertEquals(false, stateRepository.isLoading)
+        // check that got same data in the state
+        Assert.assertEquals(userRepoList.size, stateRepository.userRepoList.size)
+    }
+
 }
